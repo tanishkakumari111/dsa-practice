c101c3b46903cee5ceafe5c4d5d5b0e50b925bf1
Tanishka Kumari <168574561+tanishkakumari111@users.noreply.github.com>
2025-09-30 20:19:25 +0530

    Create findTriangularSumOfAnArray.cpp

---
+++ arrays/findTriangularSumOfAnArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        while(nums.size() > 1){
+            vector<int> next;
+            for(int i = 0; i < nums.size() - 1; i++){
+                next.push_back((nums[i] + nums[i+1]) % 10);
+            }
+            nums = next;
+        }
+        return nums[0];
+    }
+};

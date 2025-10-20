f491c3baeeff346fb1e1d1ae50a26615e3f6750d
Tanishka Kumari <168574561+tanishkakumari111@users.noreply.github.com>
2025-10-20 22:38:03 +0530

    Create finalValueOfVariableAfterPerformingOperations.cpp

---
+++ Strings/finalValueOfVariableAfterPerformingOperations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (auto &op : operations) {
+            if (op[1] == '+') x++;
+            else x--;
+        }
+        return x;
+    }
+};

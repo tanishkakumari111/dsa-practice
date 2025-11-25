3d3066c204b0f81bb8ad2e45761973e877d6af11
Tanishka Kumari <168574561+tanishkakumari111@users.noreply.github.com>
2025-11-25 21:00:55 +0530

    Create smallestIntegerDivisibleByK.cpp

---
+++ arrays/smallestIntegerDivisibleByK.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int K) {
+        if (K % 2 == 0 || K % 5 == 0)
+            return -1;
+
+        int rem = 0;
+        for (int len = 1; len <= K; len++) {
+            rem = (rem * 10 + 1) % K;
+            if (rem == 0) return len;
+        }
+
+        return -1; // theoretically never reached
+    }
+};

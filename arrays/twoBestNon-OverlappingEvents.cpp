734a6b451d1a3950796831b44cba3d0b94bf3075
Tanishka Kumari <168574561+tanishkakumari111@users.noreply.github.com>
2025-12-23 14:29:36 +0530

    Create twoBestNon-OverlappingEvents.cpp

---
+++ arrays/twoBestNon-OverlappingEvents.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        int n = events.size();
+
+        // Sort by start time
+        sort(events.begin(), events.end());
+
+        // suffixMax[i] = max value from i to end
+        vector<int> suffixMax(n);
+        suffixMax[n - 1] = events[n - 1][2];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMax[i] = max(suffixMax[i + 1], events[i][2]);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currValue = events[i][2];
+
+            // Binary search for next event with start > end
+            int l = i + 1, r = n - 1;
+            int idx = -1;
+
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (events[mid][0] > events[i][1]) {
+                    idx = mid;
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+
+            if (idx != -1) {
+                currValue += suffixMax[idx];
+            }
+
+            ans = max(ans, currValue);
+        }
+
+        return ans;
+    }
+};
